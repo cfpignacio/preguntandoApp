7ad82d75dcdab41240ff4ec181723b5be8468a83
Ignacio Camillucci <ignacio.camillucci@gmail.com>
2023-06-12 20:38:27 -0300

    example 1

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,19 @@ import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-   
+  <ul>
+  <li> <button value="false">1</button></li>
+  <li> <button value="false">50</button></li>
+  <li> <button value="true">123</button></li>
+  <li> <button value="true">4</button></li>
+</ul>
   </div>
 `;
+
+const r = document.querySelectorAll<HTMLButtonElement>('button');
+
+r?.forEach((b) => {
+	b.addEventListener('click', (e) => {
+		console.log(b.value);
+	});
+});
